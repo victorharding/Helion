7ebb75758cab174abd4c7a496c9d2e178eeb3fb9
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-04-24 20:18:44 +0200

    update sitemap

--- pages/api/site-map.js
+++ pages/api/site-map.js
@@ -20,6 +20,79 @@ export default async (req, res) => {
       changefreq: 'weekly',
       priority: 1,
     },
+    {
+      url: 'https://www.helion.digital/services',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.helion.digital/services/Dedicated-Development-Team',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/services/Custom-Software-Development',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+
+     {
+       url: 'https://www.helion.digital/technology',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+
+     {
+      url: 'https://www.helion.digital/technology/front-end',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/technology/node',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/technology/php',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/technology/net',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/technology/mobile',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/technology/java',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+      {
+      url: 'https://www.helion.digital/industries',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/industries/real-estate',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+     {
+      url: 'https://www.helion.digital/industries/travel',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+      {
+      url: 'https://www.helion.digital/industries/eCommerce',
+      changefreq: 'weekly',
+      priority: 1,
+    },
+
   ];
 
   // Create a stream to write to
