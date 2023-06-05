8047a16962f401f29d3449e98c793ed775cc1aae
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-06-05 06:31:32 -0700

    Update index.jsx

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -5,8 +5,48 @@ import Footer from '../common/nav/footer'
 import Hero from '../common/hero'
 
 export default function Layout({ children }) {
+  const SchemaMarkup ={
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Helion Studio",
+  "image": "https://www.helion-studio.com/assets/logo.svg",
+  "@id": "",
+  "url": "https://www.helion-studio.com/",
+  "telephone": "06722336609",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "374 Emus Erasmus Ave",
+    "addressLocality": "Pretoria",
+    "postalCode": "0165",
+    "addressCountry": "ZA"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": -25.8160485,
+    "longitude": 28.2514558
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ],
+    "opens": "09:00",
+    "closes": "17:00"
+  },
+  "sameAs": [
+    "https://twitter.com/helion_studio",
+    "https://www.instagram.com/helion_studio_",
+    "https://www.helion-studio.com/"
+  ],
+};
+        
+        
   return (
-    <>
     <div className='bg-primary w-full overflow-hidden'>
          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth} fixed z-10 top-0 bg-white`}>
@@ -24,7 +64,12 @@ export default function Layout({ children }) {
                  <Footer />
             </div>
         </div>
+        
+        <script type="application/ld+json">
+           {JSON.stringify(SchemaMarkup)}
+        </script>
+
     </div>
-    </>
+    
   )
 }
