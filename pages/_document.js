56dbbe08bbec38b2b76fbe6e946bac118d9a1127
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-06-06 02:38:07 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,24 @@ export default class MyDocument extends Document {
             }}
           />
            {/* Facebook Pixel*/}
+          <script
+          !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1272818106699287');
+            fbq('track', 'PageView');
+          />
           
-          
+          <noscript
+          <img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1272818106699287&ev=PageView&noscript=1"
+          />
+         
         </Head>
         <body>
           <Main />
