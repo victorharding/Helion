41935b5d44c00bd7a920584cebb5f3bbd729f335
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-06-06 02:41:14 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ export default class MyDocument extends Document {
             }}
           />
            {/* Facebook Pixel*/}
-          <script
+          <script>
           !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -34,12 +34,12 @@ export default class MyDocument extends Document {
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '1272818106699287');
             fbq('track', 'PageView');
-          />
+          <script/>
           
-          <noscript
+          <noscript>
           <img height="1" width="1" style="display:none"
           src="https://www.facebook.com/tr?id=1272818106699287&ev=PageView&noscript=1"
-          />
+          <noscript/>
          
         </Head>
         <body>
