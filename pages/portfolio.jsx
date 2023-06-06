07c8b8f9f19b8df1df6533f6c1fed4d944887c75
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-06-06 02:04:48 -0700

    Update portfolio.jsx

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -4,7 +4,7 @@ import Meta from '../src/components/Meta'
 const portfolio = () =>{
     return(
         <>
-            <Meta title={"Helion Portfolio"} ogTitle={"Helion Portfolio"} ogUrl={"https://www.helion.digital/portfolio"}/> 
+            <Meta title={"Helion Studio Portfolio | Leading software development and innovation firm"} ogTitle={"Helion Studio Portfolio | Leading software development and innovation firm"} ogUrl={"https://www.helion=studio.com/portfolio"}/> 
             <Portfolio/>
         </>
     );
