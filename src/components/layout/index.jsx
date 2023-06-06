569d1203228ef60def0911a6b7e6b85cb1dd3fb8
Victor Harding <58945094+victorharding@users.noreply.github.com>
2023-06-06 02:12:49 -0700

    Update index.jsx

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -13,7 +13,6 @@ export default function Layout({ children }) {
   "@id": "",
   "url": "https://www.helion-studio.com/",
   "telephone": "06722336609",
-   "priceRange": " Free Consultation",
   "address": {
     "@type": "PostalAddress",
     "streetAddress": "374 Emus Erasmus Ave",
